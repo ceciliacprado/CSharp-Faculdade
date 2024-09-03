c2f407e837db7448c8d664507431219f5fb4ef9d
SOMACAL <crdacostacaio@gmail.com>
2024-09-02 21:43:51 -0300

    add https, ligação com localhost

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,12 @@
+// estrutura mínima para api em c#
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// funcionalidades
+// requisição - url e verbo http - nesse caso o MapGet (get) 
+app.MapGet("/", () => "Projeto API com C#");
+
+app.MapGet("/segunda", () => "segunda funcionalidade");
+
 
 app.Run();

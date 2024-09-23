c33b76b02f1c1e915b2c62756344dc97b08d1fc3
LucasDNX <andre_lucaslencina@hotmail.com>
2024-09-23 19:41:10 -0300

    teste

--- API/Models/Produto.cs
+++ API/Models/Produto.cs
@@ -1,20 +1,15 @@
 namespace API.Models;
 
-using System;
 public class Produto
 {
-    //se tiver mais de um construtor com nomes iguais os parâmetros devem ser diferentes 
     public Produto()
-    { //identificador unico global 
+    {
         Id = Guid.NewGuid().ToString();
         CriadoEm = DateTime.Now;
-
     }
-
-    public string Id { get; set; }
-    public string Nome { get; set; }
+    public string? Id { get; set; }
+    public string? Nome { get; set; }
     public double Valor { get; set; }
     public int Quantidade { get; set; }
     public DateTime CriadoEm { get; set; }
-
 }
